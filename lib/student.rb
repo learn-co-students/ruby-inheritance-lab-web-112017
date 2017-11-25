da097bfcf07d9cf54dfac09405edf2e458a64f7a
jerrybennett <jerrysbusiness@gmail.com>
2017-11-25 11:22:06 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
 
+  attr_accessor :knowledge, :string_of_k
+
+  def initialize
+    @string_of_k = string_of_k
+    @knowledge = []
+
+  end
+
+  def learn(string_of_k)
+    @knowledge << string_of_k
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
